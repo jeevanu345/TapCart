db594797facc67cc2c35c3220d0b7481b80e1388
jeevanu345 <jeevanu345@gmail.com>
2026-02-06 19:22:25 +0530

    “update”

--- app/store/layout.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-dynamic"
-
-export default function StoreSectionLayout({ children }: { children: React.ReactNode }) {
-  return children
-}
-
-
--- app/store/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function StoreIndexRedirect() {
-  redirect("/store/products")
-}
-
-
